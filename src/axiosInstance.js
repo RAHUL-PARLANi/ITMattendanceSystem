a1d476921e4d93eeef3d611ee0c93dce75f9835b
RAHUL-PARLANi <rahulparlani112@gmail.com>
2023-07-26 03:15:45 +0530

    aixos fix

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -5,8 +5,8 @@ const useAxiosInstance = () => {
   const token = useSelector((state) => state.users.value?.tokin);
   //console.log(token)
   const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/",
-    //baseURL:'https://itmapi.netlify.app/.netlify/functions/api/',
+    //baseURL: "http://localhost:5000/",
+    baseURL:'https://itmapi.netlify.app/.netlify/functions/api/',
     headers: {
       Authorization: `Bearer ${token}`,
     },
